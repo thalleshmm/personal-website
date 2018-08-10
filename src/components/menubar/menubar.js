9c5fcbef72a560f286f206b32e2a4ee2770d58a1
Thalles Higor <thalleshigor@Mac-mini-de-Thalles.local>
2018-08-10 17:26:40 -0300

    First commit

---
+++ src/components/menubar/menubar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './menubar.css';
+
+class Menubar extends Component {
+    render() {
+        return <div className="menubar">
+            <div className="menubar__ios">
+                <div className="menubar__ios__icon-holder">
+
+                </div>
+                <div className="menubar__ios__icon-holder">
+                    
+                </div>
+            </div>
+            <div className="menubar__macos">
+
+            </div>
+        </div>
+    }
+}
+
+export default Menubar;
