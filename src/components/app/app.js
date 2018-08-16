8536e24734021e9d809eb098d5d702b875a62e20
Thalles Maia <thalleshmmaia@gmail.com>
2018-08-15 22:57:19 -0300

    App animates when it opens and when it closes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './app.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.close = this.close.bind(this);
+    }
+
+    componentDidMount() {
+        // We have to run it asyncly
+        // otherwise the css animation won't run
+        setTimeout(() => {
+            this.$wrapper.classList.add('app--active');
+        }, 32);
+    }
+
+    componentWillUnmount() {
+        this.$wrapper.classList.remove('app--active');
+    }
+
+    close() {
+        this.$wrapper.classList.remove('app--active');
+        
+        // Wait animation to end before
+        // changing the URL
+        setTimeout(() => {
+            if (this.props.history)
+                this.props.history.push('/');
+            else
+                window.history.back();
+        }, 333)
+    }
+
     render() {
-        return <div className="app">
+        const Wrapper = props => {
+            return this.props.type === 'form' ?
+                <form {...props} ref={$el => this.$wrapper = $el}>{props.children}</form> :
+                <div {...props} ref={$el => this.$wrapper = $el}>{props.children}</div>;
+        }
+
+        return <Wrapper className='app'>
             <header className="app__header">
                 <div className="app__header__actions">
-                    <Link to="/" className="app__header__actions__action">
+                    <button type="button"
+                            onClick={this.close}
+                            className="app__header__actions__action">
                         &lt; Return
-                    </Link>
+                    </button>
                     {this.props.action ?
                     <button onClick={this.props.action.onClick}
-                            className="app__header__actions__action">
+                            disabled={this.props.action.disabled ? true : false}
+                            className="app__header__actions__action"
+                            type="submit">
                         {this.props.action.text}
                     </button> : <span />}
                 </div>
@@ -24,17 +64,20 @@ class App extends Component {
             <main className="app__content">
                 {this.props.children}
             </main>
-        </div>
+        </Wrapper>
     }
 }
 
-App.propType = {
+App.propTypes = {
     title: PropTypes.string,
     children: PropTypes.element.isRequired,
     action: PropTypes.shape({
         onClick: PropTypes.func,
-        text: PropTypes.string
-    })
+        text: PropTypes.string,
+        disabled: PropTypes.bool
+    }),
+    type: PropTypes.oneOf(['default', 'form']),
+    history: PropTypes.object
 }
 
 export default App;
