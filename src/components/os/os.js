b72bd8c5a8d2476e13abb559fb0c8075566ee7a7
Thalles Maia <thalleshmmaia@gmail.com>
2018-08-15 21:22:03 -0300

    Focused elements will only be outlined if user is navigation by keyboard

--- src/components/os/os.js
+++ src/components/os/os.js
@@ -7,9 +7,50 @@ import AppMail from '../app-mail/app-mail';
 import './os.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onKeyDown = this.onKeyDown.bind(this);
+        this.onMouseMove = this.onMouseMove.bind(this);
+        this.onTouchStart = this.onTouchStart.bind(this);
+    }
+
+    state = {
+        keyboardGuided: false
+    }
+
+    componentDidMount() {
+        window.addEventListener('keydown', this.onKeyDown);
+        window.addEventListener('mousemove', this.onMouseMove);
+        window.addEventListener('touchstart', this.onTouchStart);
+    }
+
+    componentWillMount() {
+        window.removeEventListener('keydown', this.onKeyDown);
+        window.removeEventListener('mousemove', this.onMouseMove);
+        window.removeEventListener('touchstart', this.onTouchStart);
+    }
+
+    onKeyDown(evt) {
+        if (evt.key !== "Tab" || this.state.keyboardGuided === true)
+            return;
+
+        this.setState({ keyboardGuided: true });
+    }
+
+    onMouseMove() {
+        if ( ! this.state.keyboardGuided) return;
+        this.setState({ keyboardGuided: false });
+    }
+
+    onTouchStart() {
+        if ( ! this.state.keyboardGuided) return;
+        this.setState({ keyboardGuided: false });
+    }
+
     render() {
         return <BrowserRouter>
-            <div className="os">
+            <div className={`os ${this.state.keyboardGuided ? 'os--keyboard-guided' : ''}`}>
                 <Menubar />
                 <div className="os__desktop">
                     <Desktop />
